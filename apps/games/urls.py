c928e8e19f4d38df7267c8d049e4441258ab0b57
HariNivasan C <120179086+harinivasanc2000@users.noreply.github.com>
2026-08-18 17:07:48 +0100

    Add games app URLs

---
+++ apps/games/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "games"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("game/<slug:slug>/", views.game_compare, name="compare"),
+]
